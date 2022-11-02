63538b6792632da6d3174d0f6b70e8f5dbc8e3c3
swapna <swapnagovindaayya@gmail.com>
2022-11-02 11:43:52 +0530

    please check my code

---
+++ prime.js
@@ -0,0 +1,11 @@
+function identifyPrime(num) {
+
+    let flag="Yes";
+    for(let i=i;i<num;i++){
+        if(i%i===0){
+            flag=" ";
+            break;
+        }
+    }
+    console.log(flag);
+ }
